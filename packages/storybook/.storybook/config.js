c1f68b264ab59db9aeb929cdb394d5bd1de78c25
nekosaur <albert@kaaman.nu>
2019-08-09 12:26:58 +0200

    wip: added vue-storybook for sfc support

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,20 +1,33 @@
-import { configure, addDecorator } from '@storybook/vue';
-import { withKnobs } from '@storybook/addon-knobs';
-import { withA11y } from '@storybook/addon-a11y';
-import { withTemplate } from '~storybook/addon-show-vue-markup';
-import { withVuetify } from '~storybook/decorator';
+import { configure, addDecorator, storiesOf } from '@storybook/vue'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withA11y } from '@storybook/addon-a11y'
+import { withTemplate } from '~storybook/addon-show-vue-markup'
+import { withVuetify } from '~storybook/decorator'
+import { registerStories } from 'vue-storybook'
 
 addDecorator(withTemplate)
 addDecorator(withKnobs)
 addDecorator(withA11y)
 addDecorator(withVuetify)
 
-const req = require.context('../stories/', true, /index\.js$/)
+// const req = require.context('../stories/', true, /index\.js$/)
+
+// function loadStories() {
+//   const keys = req.keys()
+//   for (let i = 0; i < keys.length; i++) {
+//     req(keys[i])
+//   }
+// }
+
+// configure(loadStories, module)
+
+const req = require.context('../stories', true, /\.vue$/)
 
 function loadStories() {
   const keys = req.keys()
   for (let i = 0; i < keys.length; i++) {
-    req(keys[i])
+    const filename = keys[i]
+    registerStories(req, filename, storiesOf, {})
   }
 }
 
--- packages/storybook/.storybook/webpack.config.js
+++ packages/storybook/.storybook/webpack.config.js
@@ -1,28 +1,12 @@
-// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const path = require('path');
 
 module.exports = async ({ config }) => {
   config.resolve.alias['~storybook'] = path.resolve(__dirname)
 
-  // config.module.rules.push({
-  //   test: /\.vue$/,
-  //   loader: 'storybook-addon-vue-info/loader',
-  //   enforce: 'post'
-  // });
-
-  // config.module.rules.push({
-  //   test: /\.s[ac]ss$/,
-  //   use: [
-  //     'style-loader',
-  //     'css-loader',
-  //     {
-  //       loader: 'sass-loader',
-  //       options: {
-  //         implementation: require('sass')
-  //       }
-  //     }
-  //   ]
-  // });
+  config.module.rules.push({
+    resourceQuery: /blockType=story/,
+    loader: 'vue-storybook'
+  })
 
   return config;
 };
