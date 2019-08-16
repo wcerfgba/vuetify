bdabb68427369339c784f297e9a143c7c65700fb
nekosaur <albert@kaaman.nu>
2019-08-16 19:36:19 +0200

    fix(VDataTable): update expand and select class names

--- packages/vuetify/src/components/VDataTable/VDataTable.ts
+++ packages/vuetify/src/components/VDataTable/VDataTable.ts
@@ -376,11 +376,11 @@ export default VDataIterator.extend({
     genDefaultExpandedRow (item: any): VNode {
       const isExpanded = this.isExpanded(item)
       const classes = {
-        'expanded expanded__row': isExpanded,
+        'v-data-table__expanded v-data-table__expanded__row': isExpanded,
       }
       const headerRow = this.genDefaultSimpleRow(item, classes)
       const expandedRow = this.$createElement('tr', {
-        staticClass: 'expanded expanded__content',
+        staticClass: 'v-data-table__expanded v-data-table__expanded__content',
       }, [this.$scopedSlots['expanded-item']!({ item, headers: this.computedHeaders })])
 
       return this.$createElement(RowGroup, {
@@ -430,7 +430,7 @@ export default VDataIterator.extend({
         key: getObjectValueByPath(item, this.itemKey),
         class: {
           ...classes,
-          selected: data.isSelected,
+          'v-data-table__selected': data.isSelected,
         },
         props: {
           headers: this.computedHeaders,
